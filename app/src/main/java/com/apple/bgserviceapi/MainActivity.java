5583fab3d74716f484b08880ed1a23775b4abfdd
Tradezap Apple1 <apple1@Tradezaps-iMac-Developer.local>
2016-12-22 14:48:17 +0530

    Add Project

---
+++ app/src/main/java/com/apple/bgserviceapi/MainActivity.java
@@ -0,0 +1,14 @@
+package com.apple.bgserviceapi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    //karti.cutelad@gmail.com
+}
